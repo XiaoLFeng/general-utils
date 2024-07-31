7ec688cb96a8721872c6e314ce124c7ee940c007
XiaoLFeng <gm@x-lf.cn>
2024-07-31 16:57:16 +0800

    feat(util): 调整 Map 转换 实体的问题

--- src/main/java/com/xlf/utility/util/ConvertUtil.java
+++ src/main/java/com/xlf/utility/util/ConvertUtil.java
@@ -1,8 +1,9 @@
 package com.xlf.utility.util;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,12 +12,12 @@ import java.util.Map;
  * <br/>
  * 转换工具类
  * <p>
- *     用于将对象转换为 Map
+ * 用于将对象转换为 Map
  * </p>
  *
- * @since v1.0.0
- * @version v1.0.0
  * @author xiao_lfeng
+ * @version v1.0.0
+ * @since v1.0.0
  */
 public class ConvertUtil {
     /**
@@ -30,17 +31,9 @@ public class ConvertUtil {
      */
     @NotNull
     public static Map<String, Object> convertObjectToMap(@NotNull Object obj) {
-        Map<String, Object> map = new HashMap<>();
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            try {
-                map.put(field.getName(), field.get(obj));
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Failed to convert object to map", e);
-            }
-        }
-        return map;
+        String getJson = new Gson().toJson(obj);
+        return new Gson().fromJson(getJson, new TypeToken<HashMap<String, Object>>() {
+        }.getType());
     }
 
     /**
@@ -54,17 +47,9 @@ public class ConvertUtil {
      */
     @NotNull
     public static Map<String, String> convertObjectToMapString(@NotNull Object obj) {
-        Map<String, String> map = new HashMap<>();
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            try {
-                map.put(field.getName(), field.get(obj).toString());
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Failed to convert object to map", e);
-            }
-        }
-        return map;
+        String getJson = new Gson().toJson(obj);
+        return new Gson().fromJson(getJson, new TypeToken<Map<String, String>>() {
+        }.getType());
     }
 
     /**
@@ -78,17 +63,9 @@ public class ConvertUtil {
      */
     @NotNull
     public static Map<Object, Object> convertObjectToMapObject(@NotNull Object obj) {
-        Map<Object, Object> map = new HashMap<>();
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            try {
-                map.put(field.getName(), field.get(obj));
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Failed to convert object to map", e);
-            }
-        }
-        return map;
+        String getJson = new Gson().toJson(obj);
+        return new Gson().fromJson(getJson, new TypeToken<Map<Object, Object>>() {
+        }.getType());
     }
 
     /**
@@ -97,23 +74,13 @@ public class ConvertUtil {
      * 将 Map 转换为对象；
      * 该方法将 Map 转换为对象，该 Map 的 key 为对象的属性名，value 为对象的属性值
      *
-     * @param map Map
+     * @param map   Map
      * @param clazz 对象的 Class
-     * @param <T> 对象的类型
+     * @param <T>   对象的类型
      * @return 对象
      */
     public static <T> T convertMapToObject(Map<Object, Object> map, @NotNull Class<T> clazz) {
-        T obj = null;
-        try {
-            obj = clazz.newInstance();
-            Field[] fields = obj.getClass().getDeclaredFields();
-            for (Field field : fields) {
-                field.setAccessible(true);
-                field.set(obj, map.get(field.getName()));
-            }
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return obj;
+        String getJson = new Gson().toJson(map);
+        return new Gson().fromJson(getJson, clazz);
     }
 }
